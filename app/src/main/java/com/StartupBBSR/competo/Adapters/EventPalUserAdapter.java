72d906890282d9c97507b9af7d6d2facf4664603
swayam verma <61063154+swayamverma412@users.noreply.github.com>
2021-06-24 00:10:06 +0530

    Revert "Search Implemented"
    This reverts commit ac763ad4b2bc3e6b7ffc9ec4d308bdc007def0a3.

--- app/src/main/java/com/StartupBBSR/competo/Adapters/EventPalUserAdapter.java
+++ app/src/main/java/com/StartupBBSR/competo/Adapters/EventPalUserAdapter.java
@@ -9,7 +9,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.StartupBBSR.competo.Models.EventPalModel;
-import com.StartupBBSR.competo.Models.UserModel;
 import com.StartupBBSR.competo.databinding.EventPalUserItemBinding;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
@@ -34,24 +33,8 @@ public class EventPalUserAdapter extends FirestoreRecyclerAdapter<EventPalModel,
     //    Listener Member Variable
     private OnItemClickListener listener;
 
-    public EventPalUserAdapter (FirestoreRecyclerOptions<UserModel> Options) {
-        super((FirestoreRecyclerOptions<EventPalModel>) getOptions(Options));
-        this.listener= listener;
-
-    }
-
-    private static Object getOptions(FirestoreRecyclerOptions<UserModel> Options) {
-        return Options;
-    }
-
-    public void updateOptions (FirestoreRecyclerOptions<EventPalModel> Options) {
-    }
-
-
     //    Listener Interface
     public interface OnItemClickListener {
-
-        void onItemClick(View itemView, int position);
 
         void onButtonClick(DocumentSnapshot snapshot);
 
@@ -108,18 +91,6 @@ public class EventPalUserAdapter extends FirestoreRecyclerAdapter<EventPalModel,
             recyclerView = binding.eventPalUserSkillRecyclerView;
             btnSendMessageRequestEventPal = binding.btnSendMessage;
 
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (listener != null) {
-                        int position = getAdapterPosition();
-                        if (position != RecyclerView.NO_POSITION) {
-                            listener.onItemClick(itemView, position);
-                        }
-                    }
-                }
-            });
 
             binding.btnBottomSheet.setOnClickListener(new View.OnClickListener() {
                 @Override
